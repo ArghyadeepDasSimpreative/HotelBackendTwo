26fd4938a2138f7655488e60447d2d9f4be2707e
Rajib-Scweb <dev11.scwt@gmail.com>
2025-06-13 13:06:28 +0530

    push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/connection.js";
+import authRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
@@ -14,9 +15,7 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/test", (req, res) => {
-  res.json({ success: true, message: "Test route working" });
-});
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
